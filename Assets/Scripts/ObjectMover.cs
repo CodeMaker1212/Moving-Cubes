dc7c4a59d893e5cc7d69a3d288191a31377aed44
Dmitry <hungrybrain@yandex.ru>
2022-10-12 15:24:06 +0300

    Update ObjectMover.cs

--- Assets/Scripts/ObjectMover.cs
+++ Assets/Scripts/ObjectMover.cs
@@ -7,7 +7,7 @@ using UnityEngine.Events;
 public class ObjectMover : MonoBehaviour
 {
     [SerializeField][Range(0f, 100f)] private float _speed = 5f;
-    [SerializeField] private float _distance = 10f;
+    [SerializeField][Range(0f, 10f)] private float _distance = 10f;
     private Vector3 _targetPosition;
 
     public event UnityAction TargetDistanceCovered;
@@ -17,8 +17,14 @@ public class ObjectMover : MonoBehaviour
         get => _speed * Time.deltaTime;
         private set => _speed = Mathf.Clamp(value, 0, float.MaxValue);
     }
+
+    public float Distance
+    {
+        get => _distance;
+        private set => _distance = Mathf.Clamp(value, 0, float.MaxValue);
+    }
      
-    private void OnEnable() => _targetPosition = transform.position + Vector3.forward * _distance;
+    private void OnEnable() => _targetPosition = transform.position + Vector3.forward * Distance;
 
     private void Update()
     {
